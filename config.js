6857e61e765c9dc3b4026739e4eff8020f212dda
Carlos Del Río <karlosdelrio94@gmail.com>
2021-02-28 15:57:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 8000,
     db: process.env.MONGODB || 'mongodb://localhost:27017/curso-youtube',
-    SECRET_TOKEN: 'f5396f197770d08fc0866e7189de4f49'
+    SECRET_TOKEN: 'xxx'
 }
